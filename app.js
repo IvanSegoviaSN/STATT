47e884782e59cdb5c8775cdf18f2fcc2c6eaba85
IvanSegoviaSN <ivansegsanz@gmail.com>
2021-04-29 15:58:13 +0200

    add

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const http = require('http')
+const mongoose = require('mongoose');
+const userRouter = require('./user.route');
 
 const port = process.env.PORT || 3000
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200
     res.setHeader('Content-Type', 'text/html')
-    res.end('<h1>Hellosr, World!</h1>')
+    res.end('<h1>Hellor, World!</h1>')
 })
+
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+});
 
 server.listen(port, () => {
     console.log(`Server running at port ${port}`)
