640bdb1795012394f1c5852846d1a696103554ed
IvanSegoviaSN <ivansegsanz@gmail.com>
2021-04-29 15:38:46 +0200

    add

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const http = require('http')
 
 const port = process.env.PORT || 3000
-console.log(process.env.MONGODB_URI)
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200
@@ -9,6 +8,23 @@ const server = http.createServer((req, res) => {
     res.end('<h1>Hellor, World!</h1>')
 })
 
-server.listen(port, () => {
-    console.log(`Server running at port ${port}`)
-})
+const express = require('express');
+const mongoose = require('mongoose');
+const userRouter = require('./user.route');
+
+const app = express();
+
+app.use(express.json());
+app.use('/user', userRouter);
+
+const uri = "mongodb+srv://test:123@cluster0-vnelr.gcp.mongodb.net/test?retryWrites=true;"
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
