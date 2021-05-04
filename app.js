8ac84442f4f2e82312e4156702f69537b6f44131
IvanSegoviaSN <ivansegsanz@gmail.com>
2021-05-04 14:26:25 +0200

    commit update

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 
 const express = require("express");
-const mongoose = require('mongoose');
-const userRouter = require('./user.route');
 
 const server = express();
 
@@ -11,17 +9,6 @@ server.get('/', (req, res) => {
   res.send("Hello");
 });
 
-server.use(express.json());
-server.use('/user', userRouter);
-
-const uri = "mongodb+srv://brawlUser:wDAM7MeQcEg1SmUL@cluster0.wakn5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-
-mongoose.connect(process.env.MONGODB_URI || uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
-const connection = mongoose.connection;
-
-connection.once('open', () => {
-    console.log("MongoDB database connection established successfully");
-});
 
 server.listen(port, () => {
     console.log(`Server running at port ${port}`)
