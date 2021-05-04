c376297a993bd0cc7c54073ea8264b125b7ffa75
IvanSegoviaSN <ivansegsanz@gmail.com>
2021-05-04 16:45:14 +0200

    name update

--- user.route.js
+++ user.route.js
@@ -3,7 +3,7 @@ let User = require('./user.model');
 
 router.route('/').get((req, res) => {
     User.find()
-        .then(users => res.json(users))
+        .then(users => res.send(users))
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
