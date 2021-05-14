896c2992a9c8b6febb9231c1338edd2e32b417e6
IvanSegoviaSN <ivansegsanz@gmail.com>
2021-05-14 13:59:19 +0200

    Cambiado bearer de brawlAPIRoutes.js

--- src/routes/games/brawlAPIRoutes.js
+++ src/routes/games/brawlAPIRoutes.js
@@ -18,7 +18,7 @@ router.get('/:tag', async (req, res) => {
         url: `https://api.brawlstars.com/v1/players/%20${req.params.tag}`,
         method: "GET",
         headers: {
-            Authorization: "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjMyZGZhZGE4LTk5YjUtNDE2Yi1hYWM3LWM4NjZmOTYzMzg1NyIsImlhdCI6MTYyMDI0ODA3Miwic3ViIjoiZGV2ZWxvcGVyLzc5NmI4ODU5LWNiOGMtMGI2Yi05NDZjLWMyYzY4NzgzMjc3MiIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiNzkuMTQ2LjE3OC41OCIsIjIwNi4xODkuMjA1LjI1MSJdLCJ0eXBlIjoiY2xpZW50In1dfQ.a5O8T3JMP7hlbUqzFQmGYrr89Li0fLbQvnfmQjzyMkAqzZ7ArkycsN4JRqpxapsbHXIgS5VKsTHDuMRb4SAp-g"
+            Authorization: "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6Ijk2ZTVmZDQ3LTJhNTUtNGJlYS04YWZiLTU2ZTI1YTA0MDNiOCIsImlhdCI6MTYyMDk5MzQ4NCwic3ViIjoiZGV2ZWxvcGVyLzc5NmI4ODU5LWNiOGMtMGI2Yi05NDZjLWMyYzY4NzgzMjc3MiIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiMTA4LjEyOC4xMjAuMzEiLCI3OS4xNDguODMuMTU2Il0sInR5cGUiOiJjbGllbnQifV19.5xUHnUCa9_K0KTpFHbPQoy3dZAeShWNME90dqnGXshp9srjFFrBN5Qhu3gzH0tKmASrPVgWQS0U-QtLjCmkk6A"
         }
     }).catch(err => {
         next = false;
