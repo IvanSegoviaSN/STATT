8c07a37eeb08f784d429456b2a1014b2928d1532
IvanSegoviaSN <ivansegsanz@gmail.com>
2021-05-21 10:25:24 +0200

    Testing #1-fix

--- src/routes/linkapi/link-apiRoutes.js
+++ src/routes/linkapi/link-apiRoutes.js
@@ -7,7 +7,8 @@ router.get('/:id', async (req, res) => {
         'Content-Type': 'application/json'
     }
     // https://api.statt.es/api/games/brawl/${req.params.id}`
-    const response = await axios(`http://localhost:3001/api/games/brawl/${req.params.id}`).catch(err => res.send('404'));
+    const response = await axios(`http://localhost:3001/api/games/brawl/${req.params.id}` || `https://api.statt.es/api/games/brawl/${req.params.id}`)
+        .catch(err => res.send('404'));
 
     console.log(response.data)
     await res.send(response.data);
